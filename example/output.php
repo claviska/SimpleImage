8194e630442fef17f85cc97bcd0c301dc6f76e4b
Cory LaViska <cory@abeautifulsite.net>
2013-01-19 18:53:05 -0500

    Updated example to use method chaining for better readability

--- example/output.php
+++ example/output.php
@@ -6,13 +6,9 @@ if( !is_dir('processed/') ) mkdir('processed/');
 
 try {
 	
+	// Flip the image and output it directly to the browser
 	$img = new SimpleImage();
-	
-	$img->load('butterfly.jpg');
-	$img->overlay('overlay.png', 'center center', .8);	
-	$img->text('Butterfly', 'delicious.ttf', 32, '#00f', 'bottom', 0, -20);
-
-	$img->output(100);
+	$img->load('butterfly.jpg')->flip('x')->output('png');
 	
 } catch(Exception $e) {
 	
