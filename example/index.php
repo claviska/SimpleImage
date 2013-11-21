f5b9cfbfea753852e097d883419eddfb0dab9289
mikedidomizio <mikedidomizio@gmail.com>
2013-11-21 10:34:46 -0500

    Added the ability to manipulate base64 image strings
    Allows the user to input a base64 image string instead of image.  User
    could modify the image using <canvas>, upload the client side
    manipulated base64 string and go from there.  Requires PHP 5.4 for
    getimagesizefromstring
    
    How to use :
    
    $img->load_base64($base64string)->resize(32,32)->output();

--- example/index.php
+++ example/index.php
@@ -23,7 +23,7 @@ try {
 
 	// Strip exif data (just load and save)
 	$img->load('butterfly.jpg')->save('processed/butterfly-strip-exif.jpg');
-
+	
 	// Flip horizontal
 	$img->load('butterfly.jpg')->flip('x')->save('processed/butterfly-flip-horizontal.jpg');
 
@@ -113,6 +113,10 @@ try {
 
 	// Resizing GIFs with transparency
 	$img->load('basketball.gif')->resize(50, 50)->save('processed/basketball-resize.gif');
+	
+	// Manipulate base64 gif string and save as png
+	$base64 = "data:image/gif;base64,R0lGODlhEAAQAOZeAHBwcKCgoOraIvDw8Mu9Hi4rBvPFJvTKJpyRF/bXJfPAJ/jeJU5IC0BAQOq0J+ewKPnmJffZJVc6FvPCJruuGz46CYyDFPXPJpCQkH10EqugGSYaC+GoKAAAALCwsNicKUlGQvbUJtGaKWBgYODg4LWBJDUnF/jhJa9+JXFgSl4/GUxHQjwpEdadKUxDOEM9NmNdVcaPJ9rLIPbRJtifKUEwGuSsKNmhKV1XDXdPG6p2JXhRG1U5FiMdFlpWU76DJ8DAwMuVKtDQ0GlFFrR8JEQtELF8KO24J3VNGT0zKG1lEIxgHdugKXBNGykbCzw3Mt6kKNOcKfXMJt6mKG5LG2JAFTgwJW5fSlNNRenSI/THJo5iIPC8J/rpJf///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAF4ALAAAAAAQABAAAAfZgF6CXiQAIBsbSSNAg40YTlstDw8cMVUrA40jKjcKWiEhMwZHRCYkggE8UxMLXa6uCVw/T14DPVETWQwarhoWXRFcOwEBTVwJCB0VrhUdFF0HNC8AKAoQAhkErggMsA5FDSIHr+SvCwoS4QcUAuVdBBYR6AAlBhYZ5QIFBBcOLB45uJwogOAVAYIQJgSB4cXKhwMCcBRQwqCCNilcqATw4mEIhwtdBFCQ0QXCBS5GQAwCgISJgQgLFiQw4ECHi0yDAtRY8sHGAyglJPjA2WgAhgZXUmABIKRRIAA7";
+	$img->load_base64($base64)->resize(32,32)->save('processed/smiley-base64.png');
 	
 	echo '<span style="color: green;">All processed images are saved in /example/processed</span>';
 
