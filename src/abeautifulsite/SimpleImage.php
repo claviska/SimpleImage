0274cf46ba80eb69f2edf344eaa03f0037196c95
Cory LaViska <cory@abeautifulsite.net>
2015-03-11 20:37:18 -0400

    Removed period

--- src/abeautifulsite/SimpleImage.php
+++ src/abeautifulsite/SimpleImage.php
@@ -5,7 +5,7 @@
  * @author      Cory LaViska for A Beautiful Site, LLC (http://www.abeautifulsite.net/)
  * @author      Nazar Mokrynskyi <nazar@mokrynskyi.com> - merging of forks, namespace support, PhpDoc editing, adaptive_resize() method, other fixes
  * @license     This software is licensed under the MIT license: http://opensource.org/licenses/MIT
- * @copyright   A Beautiful Site, LLC.
+ * @copyright   A Beautiful Site, LLC
  *
  */
 
