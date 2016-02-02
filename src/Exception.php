c50ac900a5769bc6022cfb5eddd82df5a1540193
SmetDenis <denis@jbzoo.com>
2016-02-02 21:08:14 +0600

    Add exception file

---
+++ src/Exception.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * JBZoo Image
+ *
+ * This file is part of the JBZoo CCK package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package   Image
+ * @license   MIT
+ * @copyright Copyright (C) JBZoo.com,  All rights reserved.
+ * @link      https://github.com/JBZoo/Image
+ */
+
+namespace JBZoo\Image;
+
+/**
+ * Class Exception
+ * @package JBZoo\Image
+ */
+class Exception extends \Exception
+{
+    
+}
