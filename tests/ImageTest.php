ce55c78bf7f2fe4264f143e6a915b179409f57f0
hikouki <ame.kh000@gmail.com>
2016-03-04 19:44:16 +0900

    Add test for invalid image string.

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -434,4 +434,21 @@ class ImageTest extends PHPUnit
             isTrue(strpos(PHP_VERSION, '5.3') === 0);
         }
     }
+
+    public function testInvalidImageString()
+    {
+        try {
+            $img = new Image();
+            $img->loadString('__INVALID__', true);
+        } catch(Exception $e) {
+            isTrue($e->getMessage() === 'Invalid image source.');
+        }
+
+        try {
+            $img = new Image('__INVALID__', true);
+        } catch(Exception $e) {
+            isTrue($e->getMessage() === 'Invalid image source.');
+        }
+    }
+
 }
