cdb13522c0a6479f59b75ef2cf595cf9b9513d0c
Cory LaViska <cory@abeautifulsite.net>
2017-01-06 10:09:13 -0500

    Version bump

--- src/abeautifulsite/SimpleImage.php
+++ src/abeautifulsite/SimpleImage.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     SimpleImage class
- * @version     2.6.0
+ * @version     2.6.1
  * @author      Cory LaViska for A Beautiful Site, LLC (http://www.abeautifulsite.net/)
  * @author      Nazar Mokrynskyi <nazar@mokrynskyi.com> - merging of forks, namespace support, PhpDoc editing, adaptive_resize() method, other fixes
  * @license     This software is licensed under the MIT license: http://opensource.org/licenses/MIT
