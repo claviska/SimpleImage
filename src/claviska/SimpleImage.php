21696b568704b8cc0b7d734ae15e816a371ab016
maPer77 <ciganox@yahoo.com.br>
2021-02-02 09:11:57 -0300

    Fixes problem with red border in ellipse, arc and roundedRectangle

--- src/claviska/SimpleImage.php
+++ src/claviska/SimpleImage.php
@@ -1361,7 +1361,7 @@ class SimpleImage {
       imagefilledarc($tempImage->image, $x, $y, $width+$thickness, $height+$thickness, $start, $end, $tempColor, IMG_ARC_PIE);
 
       // Draw a smaller ellipse filled with red|blue (-$thickness pixels)
-      $tempColor = ($color == 'red') ? 'blue' : 'red';
+      $tempColor = (self::normalizeColor($color)['red'] == 255) ? 'blue' : 'red';
       $tempColor = $tempImage->allocateColor($tempColor);
       imagefilledarc($tempImage->image, $x, $y, $width-$thickness, $height-$thickness, $start, $end, $tempColor, IMG_ARC_PIE);
 
@@ -1443,7 +1443,7 @@ class SimpleImage {
       imagefilledellipse($tempImage->image, $x, $y, $width+$thickness, $height+$thickness, $tempColor);
 
       // Draw a smaller ellipse filled with red|blue (-$thickness pixels)
-      $tempColor = ($color == 'red') ? 'blue' : 'red';
+      $tempColor = (self::normalizeColor($color)['red'] == 255) ? 'blue' : 'red';
       $tempColor = $tempImage->allocateColor($tempColor);
       imagefilledellipse($tempImage->image, $x, $y, $width-$thickness, $height-$thickness, $tempColor);
 
@@ -1603,7 +1603,7 @@ class SimpleImage {
       $tempImage->roundedRectangle($x1, $y1, $x2, $y2, $radius, $color,'filled');
 
       // Draw a smaller rectangle filled with red|blue (-$thickness pixels on each side)
-      $tempColor = ($color == 'red') ? 'blue' : 'red';
+      $tempColor = (self::normalizeColor($color)['red'] == 255) ? 'blue' : 'red';
       $radius = $radius - $thickness;
       $radius = self::keepWithin($radius, 0, $radius);
       $tempImage->roundedRectangle(
