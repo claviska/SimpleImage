b6f0950a12e8928b84475c960e46a0eb2ca45340
Stefaminator <info@timm-maass.de>
2025-09-17 22:35:09 +0200

    Fix issue 342 (#343)
    * Fixes #342: Used always the same part of code "fromString" for creating images from file or from data uri
    
    * Fixes #342: improved optional mimeType param behaviour for fromString
    
    * Fixes #342: bugfix, fromString should remain public
    
    * Update src/claviska/SimpleImage.php
    
    ---------
    
    Co-authored-by: Timm Maass <timm.maass@perbility.de>
    Co-authored-by: Cory LaViska <cory@abeautifulsite.net>

--- src/claviska/SimpleImage.php
+++ src/claviska/SimpleImage.php
@@ -193,20 +193,18 @@ class SimpleImage
         }
 
         // Determine mime type
-        $this->mimeType = $matches[1];
-        if (! preg_match('/^image\/(gif|jpeg|png)$/', $this->mimeType)) {
+        $mimeType = $matches[1];
+        if (! preg_match('/^image\/(gif|jpeg|png)$/', $mimeType)) {
             throw new Exception(
-                'Unsupported format: '.$this->mimeType,
+                'Unsupported format: ' . $mimeType,
                 self::ERR_UNSUPPORTED_FORMAT
             );
         }
 
         // Get image data
-        $uri = base64_decode(strval(preg_replace('/^data:(.*?);base64,/', '', $uri)));
-        $this->image = imagecreatefromstring($uri);
-        if (! $this->image) {
-            throw new Exception('Invalid image data.', self::ERR_INVALID_IMAGE);
-        }
+        $data = base64_decode(strval(preg_replace('/^data:(.*?);base64,/', '', $uri)));
+
+        $this->fromString($data, $mimeType);
 
         return $this;
     }
@@ -221,7 +219,7 @@ class SimpleImage
      */
     public function fromFile(string $file): static
     {
-    // Set fopen options.
+        // Set fopen options.
         $sslVerify = $this->getFlag('sslVerify'); // Don't perform peer validation when true
         $opts = [
             'ssl' => [
@@ -231,20 +229,40 @@ class SimpleImage
         ];
 
         // Check if the file exists and is readable.
-        $file = @file_get_contents($file, false, stream_context_create($opts));
-        if ($file === false) {
+        $data = @file_get_contents($file, false, stream_context_create($opts));
+        if ($data === false) {
             throw new Exception("File not found: $file", self::ERR_FILE_NOT_FOUND);
         }
 
-        // Create image object from string
-        $this->image = imagecreatefromstring($file);
+        $this->fromString($data);
 
-        // Get image info
-        $info = @getimagesizefromstring($file);
-        if ($info === false) {
-            throw new Exception("Invalid image file: $file", self::ERR_INVALID_IMAGE);
+        return $this;
+    }
+
+    /**
+     * Creates a new image from an image data string.
+     *
+     * @param string $data
+     * @param string|null $mimeType
+     * @return SimpleImage
+     * @throws Exception
+     */
+    public function fromString(string $data, ?string $mimeType = null): static
+    {
+
+        if($mimeType === null) {
+            // Get image info
+            $info = @getimagesizefromstring($data);
+            if ($info === false) {
+                throw new Exception("Invalid image data", self::ERR_INVALID_IMAGE);
+            }
+            $this->mimeType = $info['mime'];
+        } else {
+            $this->mimeType = $mimeType;
         }
-        $this->mimeType = $info['mime'];
+
+        // Create image object from string
+        $this->image = imagecreatefromstring($data);
 
         if (! $this->image) {
             throw new Exception('Unsupported format: '.$this->mimeType, self::ERR_UNSUPPORTED_FORMAT);
@@ -268,12 +286,12 @@ class SimpleImage
             case 'image/jpeg':
                 // Load exif data from JPEG images
                 if (function_exists('exif_read_data')) {
-                    $this->exif = @exif_read_data('data://image/jpeg;base64,'.base64_encode($file));
+                    $this->exif = @exif_read_data('data://image/jpeg;base64,'.base64_encode($data));
                 }
                 break;
         }
 
-        // Convert pallete images to true color images
+        // Convert palette images to true color images
         imagepalettetotruecolor($this->image);
 
         return $this;
@@ -302,21 +320,6 @@ class SimpleImage
         return $this;
     }
 
-    /**
-     * Creates a new image from a string.
-     *
-     * @param  string  $string The raw image data as a string.
-     * @return SimpleImage
-     *
-     * @throws Exception
-     *
-     * @example
-     *    $string = file_get_contents('image.jpg');
-     */
-    public function fromString(string $string): SimpleImage|static
-    {
-        return $this->fromFile('data://;base64,'.base64_encode($string));
-    }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////
     // Savers
